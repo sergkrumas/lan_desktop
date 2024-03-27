3fb99601a6dbd19fc81bbc604dc5ecbeb9b80228
sergkrumas <cjkrumas@gmail.com>
2024-03-27 08:25:19 +0300

    modifing frameless widget: moving the whole widget

--- _resizable_frameless_modificated.py
+++ _resizable_frameless_modificated.py
@@ -26,6 +26,13 @@ class ResizableWidgetWindow(ResizableFramelessWindow):
         # задаю минимальный размер, чтобы пользователь нечаянно не занулил размеры
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         self.setMinimumSize(100, 100)
+        self.translating_ongoing = False
+        self.center_rect = QRect(0, 0, 50, 50)
+
+    def getCenterRect(self):
+        r = QRect(self.center_rect)
+        r.moveCenter(self.rect().center())
+        return r
 
     def paintEvent(self, event):
         painter = QPainter()
@@ -38,8 +45,39 @@ class ResizableWidgetWindow(ResizableFramelessWindow):
         r = self.rect()
         r.adjust(width_size-1, width_size-1, -width_size, -width_size)
         painter.drawRect(r)
+
+        r = self.getCenterRect()
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor(240, 240, 240, 200))
+        painter.drawRect(r)
+
         painter.end()
 
+    def mousePressEvent(self, event):
+
+        center_rect = self.getCenterRect()
+        if center_rect.contains(event.pos()):
+            self.translating_ongoing = True 
+            self.start_pos = QCursor().pos()
+            self.start_topleft = self.geometry().topLeft()
+        else:
+            self.translating_ongoing = False
+
+    def mouseMoveEvent(self, event):
+        if self.translating_ongoing:
+            # перенос всего виджета целиком
+            pos = QCursor().pos()
+            delta = self.start_pos - pos
+            geometry = self.geometry()
+            geometry.moveTopLeft(self.start_topleft - delta)
+            self.setGeometry(geometry)
+
+    def mouseReleaseEvent(self, event):
+        if self.translating_ongoing:
+            # ни за что не убирать этот код отсюда,
+            # иначе угловые прихватки начнут чудить,
+            # плюс это нужно по внутренней логике
+            self.translating_ongoing = False
 
 if __name__ == '__main__':
 
