73ec46b5e57470b68f7345e07648c5d5c2122b7c
sergkrumas <cjkrumas@gmail.com>
2024-03-27 09:46:01 +0300

    make_user_defined_capture_screenshot

--- tesst.py
+++ tesst.py
@@ -16,25 +16,11 @@ from PyQt5.QtNetwork import *
 import sys
 
 
-def make_screenshot_pyqt():
+def make_user_defined_capture_screenshot(capture_rect):
     desktop = QDesktopWidget()
-    MAX = 1000000000
-    left = MAX
-    right = -MAX
-    top = MAX
-    bottom = -MAX
-    for i in range(0, desktop.screenCount()):
-        r = desktop.screenGeometry(screen=i)
-        left = min(r.left(), left)
-        right = max(r.right(), right)
-        top = min(r.top(), top)
-        bottom = max(r.bottom(), bottom)
-    all_monitors_zone = QRect(QPoint(left, top), QPoint(right+1, bottom+1))
-
-    # print(all_monitors_zone)
     qimage = QImage(
-        all_monitors_zone.width(),
-        all_monitors_zone.height(),
+        capture_rect.width(),
+        capture_rect.height(),
         QImage.Format_RGB32
     )
     qimage.fill(Qt.black)
@@ -43,48 +29,22 @@ def make_screenshot_pyqt():
     painter.begin(qimage)
     screens = QGuiApplication.screens()
     for n, screen in enumerate(screens):
-        p = screen.grabWindow(0)
-        source_rect = QRect(QPoint(0, 0), screen.geometry().size())
-        painter.drawPixmap(screen.geometry(), p, source_rect)
+        screen_geometry = screen.geometry()
+        if screen_geometry.intersects(capture_rect):
+            screen_pixmap = screen.grabWindow(0)
+            repos = screen_geometry.topLeft() - capture_rect.topLeft()
+            screen_geometry.moveTopLeft(repos)
+            source_rect = QRect(QPoint(0, 0), screen.geometry().size())
+            painter.drawPixmap(screen_geometry, screen_pixmap, source_rect)
     painter.end()
     return qimage
 
+if __name__ == '__main__':
 
+    app = QApplication(sys.argv)
 
-app = QApplication(sys.argv)
+    input_rect = QRect(2360, 0, 500, 400)
+    input_rect = QRect(0, 0, 2560*2, 1440)
+    input_rect = QRect(2560, 0, 2560, 1440)
+    make_user_defined_capture_screenshot(input_rect).save('image.jpg')
 
-
-
-# stream = QDataStream()
-# stream << make_screenshot_pyqt()
-# print(dir(stream))
-# QByteArray(stream)
-# print(stream.status())
-
-# print(make_screenshot_pyqt().bits())
-
-
-# make_screenshot_pyqt()
-def prepare_screenshot_to_transfer():
-    image = make_screenshot_pyqt()
-
-    byte_array = QByteArray()
-    buffer = QBuffer(byte_array)
-    buffer.open(QIODevice.WriteOnly)
-    image.save(buffer, "jpg")
-
-    data = byte_array.data()
-    data_length = len(data)
-    data_to_sent = data_length.to_bytes(4, 'big') + data
-
-	# testing
-    # input_byte_array = QByteArray(data)
-    # i = QImage()
-    # i.loadFromData(input_byte_array, "jpg");
-    # print(i.size())
-
-    return data_to_sent
-
-
-
-prepare_screenshot_to_transfer()
